c2f866577f6e461155f756cf162f0d19e9a9a2b7
steve is useless <76439485+stveu@users.noreply.github.com>
2022-10-02 22:25:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*var dropDownButton = document.getElementById("browseProducts");
+var dropDownActive = false
+
+dropDownButton.addEventListener("click", function(){
+    if (!dropDownActive) {
+        $('.nav-menu').css('display', 'flex')
+        $('.nav-menu').animate({
+            top: '80px',
+            opacity: '1.0'
+        });
+        dropDownActive = true
+    } else {
+        $('.nav-menu').animate({
+            top: '-40px',
+            opacity: '0.0'
+        });
+        setTimeout(hideDropDown, 1000);
+        dropDownActive = false
+    }
+});
+
+function hideDropDown() {
+    $('.nav-menu').css('display', 'none')
+}*/
